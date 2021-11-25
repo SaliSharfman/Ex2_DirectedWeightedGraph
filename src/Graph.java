c57504225372cd4dd95fc8accd1f0ecec9b0e00a
SaliSharfman <salishar2@gmail.com>
2021-11-25 23:34:50 +0200

    commit

--- src/Graph.java
+++ src/Graph.java
@@ -7,66 +7,84 @@ import java.util.Iterator;
 
 public class Graph implements DirectedWeightedGraph {
 
-    private HashMap<String,NodeData> nodes;
-    private HashMap<String,EdgeData> edges;
+    private HashMap<Integer,NodeData> nodes;
+    private HashMap<Integer,HashMap<Integer,EdgeData>> edges;
+    private static int Edgescount=0;
     public Graph() {
-        nodes=new HashMap<String,NodeData>();
-        edges=new HashMap<String,EdgeData>();
+        nodes=new HashMap<Integer,NodeData>();
+     //   edgesOfSrc=new HashMap<Integer,EdgeData>();
+        edges=new HashMap<Integer,HashMap<Integer,EdgeData>>();
     }
     @Override
     public NodeData getNode(int key){
-       return this.nodes.get(Integer.toString(key));
+       return this.nodes.get(key);
     }
     @Override
     public EdgeData getEdge(int src, int dest)
     {
-        String str =Integer.toString(src)+","+Integer.toString(dest);
-        return this.edges.get(str);
+        return this.edges.get(src).get(dest);
     }
     @Override
     public void addNode(NodeData n)
     {
-        this.nodes.put(Integer.toString(n.getKey()),n);
+        this.nodes.put(n.getKey(),n);
     }
     @Override
     public void connect(int src, int dest, double w){
-        if(nodes.get(Integer.toString(src))!=null&&nodes.get(Integer.toString(src))!=null) {
-            String str = Integer.toString(src) + "," + Integer.toString(dest);
+        if(nodes.get(src)!=null&&nodes.get(src)!=null) {
             EdgeData e = new Edge(src,dest,w);
-            edges.put(str,e);
+            edges.get(src).put(dest,e);
+            Edgescount++;
         }
     }
     @Override
     public Iterator<NodeData> nodeIter()
     {
-        Set set = nodes.entrySet();
-        Iterator<NodeData> iterator = set.iterator();
-        return iterator;
+        return nodes.values().iterator();
     }
+    @Override
     public Iterator<EdgeData> edgeIter(){
-        Set set = edges.entrySet();
-        Iterator<EdgeData> iterator = set.iterator();
-        return iterator;
+        LinkedList<EdgeData>list=new LinkedList<>();
+        for(int i:edges.keySet())
+            for(int j:edges.get(i).keySet())
+                list.add(edges.get(i).get(j));
+        return list.iterator();
     }
+    @Override
     public Iterator<EdgeData> edgeIter(int node_id)
     {
-
+        return edges.get(node_id).values().iterator();
     }
     public NodeData removeNode(int key){
-        nodes.remove(Integer.toString(key));
-
+        Edgescount-=edges.get(key).size();
+        edges.remove(key);
+        for(int i:edges.keySet())
+                if(this.removeEdge(i,key)!=null)
+                    Edgescount--;
+       return nodes.remove(key);
+    }
+    @Override
+    public EdgeData removeEdge(int src, int dest)
+    {
+        Edgescount--;
+        return edges.get(src).remove(dest);
+    }
+    @Override
+    public int nodeSize()
+    {
+        return nodes.size();
+    }
+    public int edgeSize()
+    {
+        return Edgescount;
+    }
+    public int getMC()
+    {
+        return 0;
     }
     /**
 
 
-
-
-    public Iterator<EdgeData> edgeIter(int node_id);
-
-
-
-
-    public EdgeData removeEdge(int src, int dest);
 
     public int nodeSize();
 
