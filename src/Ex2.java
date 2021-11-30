94429b28380fa652eabf9e9e082c1c88bb6ad6e7
JosefSo <77780368+JosefSo@users.noreply.github.com>
2021-11-30 11:30:29 +0200

    Update Ex2.java

--- src/Ex2.java
+++ src/Ex2.java
@@ -41,8 +41,7 @@ public class Ex2 {
         // ********************************
     }
     
-        private static void deserialize()
-    {
+    private static void deserialize() {
         try
         {
             GsonBuilder builder = new GsonBuilder();
