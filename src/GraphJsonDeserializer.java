70cd7f744e63976e21c671d87566ffe66aea73a4
JosefSo <77780368+JosefSo@users.noreply.github.com>
2021-12-05 18:41:48 +0200

    Update GraphJsonDeserializer.java

--- src/GraphJsonDeserializer.java
+++ src/GraphJsonDeserializer.java
@@ -1,36 +1,32 @@
-
 import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map.Entry;
-import java.util.Set;
-
-
+import java.util.Iterator;
 import api.DirectedWeightedGraph;
 import api.NodeData;
 import com.google.gson.*;
+import implementation.Edge;
+import implementation.Graph;
+import implementation.Node;
 
-public class GraphJsonDeserializer implements JsonDeserializer<Graph>
-{
 
-   @Override
-   public Graph deserialize(JsonElement json, Type arg1, JsonDeserializationContext arg2) throws JsonParseException
-   {
+public class GraphJsonDeserializer implements JsonDeserializer<Graph> {
+
+    @Override
+    public Graph deserialize(JsonElement json, Type arg1, JsonDeserializationContext arg2) throws JsonParseException {
         JsonObject jsonObject = json.getAsJsonObject();
         DirectedWeightedGraph graph = new Graph();
 
-        JsonArray nodesArray = (JsonArray)jsonObject.get("Nodes"); //Gets all Nodes as a json array
+        JsonArray nodesArray = (JsonArray) jsonObject.get("Nodes"); //Gets all Nodes as a json array
         Iterator<JsonElement> nodeIter = nodesArray.iterator();
 
         while (nodeIter.hasNext()) {
             JsonObject JsonNode = nodeIter.next().getAsJsonObject();
             String nodePos = JsonNode.get("pos").getAsString();
             int nodeId = JsonNode.get("id").getAsInt();
-            NodeData node = new Node(nodePos,nodeId);
+            NodeData node = new Node(nodePos, nodeId);
             graph.addNode(node);
         }
 
-        JsonArray edgesArray = (JsonArray)jsonObject.get("Edges"); //Gets all Nodes as a json array
+        JsonArray edgesArray = (JsonArray) jsonObject.get("Edges"); //Gets all Nodes as a json array
         Iterator<JsonElement> edgeIter = edgesArray.iterator();
 
         while (edgeIter.hasNext()) {
@@ -40,10 +36,10 @@ public class GraphJsonDeserializer implements JsonDeserializer<Graph>
             double edgeW = JsonNode.get("w").getAsDouble();
             int edgeDest = JsonNode.get("dest").getAsInt();
 
-            Edge edge = new Edge(edgeSrc,edgeW,edgeDest);
-            graph.connect(edgeSrc,edgeDest,edgeW);
+            Edge edge = new Edge(edgeSrc, edgeW, edgeDest);
+            graph.connect(edgeSrc, edgeDest, edgeW);
         }
         return (Graph) graph;
-
+    }
 }
 
